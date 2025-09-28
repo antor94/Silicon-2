a7d3ef3035715e5a9cde86541523d87fe82d79d5
Antor Web-Dev <antorremeni@gmail.com>
2025-09-28 13:26:25 +0600

    chnage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,11 +15,10 @@ const Banner = () => {
               {/* ---------- h2 */}
               <div className='w-full'><h2 className='text-3xl sm:text-4xl md:text-5xl font-extrabold font-main text-[#131022]'>Task Management Assistant You Gonna Love</h2></div>
               {/*------------------- description  */}
-              <div className='w-full pt-6 pb-8'><p className='text-base md:text-lg font-normal font-main text-[#585C7B]'>We offer you a new generation of task management system. Plan, manage & track all your tasks in one flexible tool.</p></div>
+              <div className='mt-[32px] mb-[48px]'><p className='text-base md:text-lg font-normal font-main text-[#585C7B]'>We offer you a new generation of task management system. Plan, manage & track all your tasks in one flexible tool.</p></div>
               {/* ------------ button */}
-              <div className='w-full max-w-xs bg-[#6366F1] rounded-[6px] shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)] mb-8'>
-                <button className='w-full py-3 text-base md:text-lg font-semibold font-main text-[#fff] rounded-[6px] px-8'>Get started for free</button>
-              </div>
+             
+                <button className='w-[210px] bg-[#6366F1] rounded-[6px] shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)]'>Get started for free</button>
               {/* --------------- subtitle */}
               <div><h2 className='text-sm md:text-base pt-12 pb-10 font-extrabold font-main text-[#131022]'>Largest companies use our tool to work efficiently</h2></div>
               {/* ------------- icons */}
